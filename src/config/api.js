fa18cc2f17cc7040f65b949155047d09c01d00d4
fhurau <sudentstar@gmail.com>
2022-11-13 22:03:13 +0700

    commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: process.env.REACT_APP_BASEURL,
+    baseURL: "https://helloo-corona.herokuapp.com/api/v1/",
 });
 
 export const setAuthToken = (token) => {
